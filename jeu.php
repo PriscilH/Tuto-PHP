40a33b42f0140b8bcbef60ab13ba822cafb65e70
Priscilla <comptepro@mbpdepriscilla.home>
2024-03-12 11:08:58 +0100

    edit condition chiffre

--- jeu.php
+++ jeu.php
@@ -5,8 +5,21 @@ require 'header.php';
 ?>
 
 <div class="container my-5">
-<form action="/jeu.php" methode="GET">
-    <input type="number" name="chiffre" placeholder="entre 0 et 1000">
-    <button type=submit>Deviner</button>
-</form>
+    <?php 
+    // Vérifier si la variable $_GET['chiffre'] est définie avant de l'utiliser
+    if (isset($_GET['chiffre'])) {
+        if ($_GET['chiffre'] > $aDeviner) : ?>
+            Votre chiffre est trop grand
+        <?php elseif ($_GET['chiffre'] < $aDeviner) : ?>
+            Votre chiffre est trop petit
+        <?php else : ?>
+            Bravo ! Vous avez deviné le chiffre <?= $aDeviner ?>
+        <?php endif ?>
+    <?php } ?>
+
+    <!-- htmlentities permet d'éviter à l'utilisateur de casser notre code html -->
+    <form action="/jeu.php" method="GET"> 
+        <input type="number" name="chiffre" placeholder="entre 0 et 1000" value="<?= isset($_GET['chiffre']) ? htmlentities($_GET['chiffre']) : '' ?>">
+        <button type="submit">Deviner</button> 
+    </form>
 </div>
