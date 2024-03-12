18414dd602ac11cb47078bacfa644a1a3cb98e21
Priscilla <comptepro@mbpdepriscilla.home>
2024-03-12 11:33:58 +0100

    add erreur and sucess alert in condition game

--- jeu.php
+++ jeu.php
@@ -1,25 +1,38 @@
 <?php
 $title = "Page du jeu";
 $aDeviner = 140;
+$erreur = null;
+$succes = null;
+$value = null;
+
+//  Vérifier si la variable $_GET['chiffre'] est définie avant de l'utiliser
+if (isset($_GET['chiffre'])) {
+    if ($_GET['chiffre'] > $aDeviner) {
+        $erreur = "Votre chiffre est trop grand";
+    } elseif ($_GET['chiffre'] < $aDeviner) {
+        $erreur = "Votre chiffre est trop petit";
+    } else {
+        $succes = "Bravo ! Vous avez deviné le chiffre <strong>$aDeviner</strong>";
+    }
+    $value = (int)$_GET['chiffre'];
+}
+
 require 'header.php';
 ?>
 
 <div class="container my-5">
-    <?php 
-    // Vérifier si la variable $_GET['chiffre'] est définie avant de l'utiliser
-    if (isset($_GET['chiffre'])) {
-        if ($_GET['chiffre'] > $aDeviner) : ?>
-            Votre chiffre est trop grand
-        <?php elseif ($_GET['chiffre'] < $aDeviner) : ?>
-            Votre chiffre est trop petit
-        <?php else : ?>
-            Bravo ! Vous avez deviné le chiffre <?= $aDeviner ?>
-        <?php endif ?>
-    <?php } ?>
+    <?php if ($erreur) : ?>
+        <div class="alert alert-danger">
+            <?= $erreur ?>
+        </div>
+    <?php elseif ($succes) : ?>
+        <div class="alert alert-success">
+            <?= $succes ?>
+        </div>
+    <?php endif ?>
 
-    <!-- htmlentities permet d'éviter à l'utilisateur de casser notre code html -->
-    <form action="/jeu.php" method="GET"> 
-        <input type="number" name="chiffre" placeholder="entre 0 et 1000" value="<?= isset($_GET['chiffre']) ? htmlentities($_GET['chiffre']) : '' ?>">
-        <button type="submit">Deviner</button> 
+    <form action="/jeu.php" method="GET">
+        <input type="number" name="chiffre" placeholder="entre 0 et 1000" value="<?= $value ?>">
+        <button type="submit">Deviner</button>
     </form>
 </div>
