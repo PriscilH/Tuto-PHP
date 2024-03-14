15b78b72b4087128327a584dffa5bbc44e584e7a
Priscilla <comptepro@mbpdepriscilla.home>
2024-03-14 13:56:56 +0100

    add creneaux d'ouvertures fonctions

--- contact.php
+++ contact.php
@@ -2,7 +2,15 @@
 $title = "Page de contact";
 require_once 'config.php';
 require_once 'functions.php';
-require 'header.php';
+date_default_timezone_set('Europe/Paris');
+// Récupérer l'heure d'aujourd'hui $heure
+$heure = (int)date('G');
+// Récupérer les créneaux d'aujourd'hui $creneaux
+$creneaux = CRENEAUX[date('N') - 1];
+// Récupérer l'état d'ouverture du magasin
+$ouvert = in_creneaux($heure, $creneaux);
+$color = $ouvert ? 'green' : 'red';
+    require 'header.php';
 ?>
 
 
@@ -14,10 +22,19 @@ require 'header.php';
         </div>
         <div class="col-md-4">
             <h2>Horaires d'ouvertures</h2>
+            <?php if ($ouvert) : ?>
+                <div class="alert alert-success">
+                    Le magasin est ouvert
+                </div>
+            <?php else : ?>
+                <div class="alert alert-danger">
+                    Le magasin est fermé
+                </div>
+            <?php endif; ?>
             <ul>
                 <?php foreach (JOURS as $k => $jour) : ?>
-                    <li>
-                        <strong><?= $jour ?></strong> 
+                    <li <?php if ($k + 1 === (int)date('N')) : ?> style="color:<?= $color; ?>" <?php endif ?>>
+                        <strong><?= $jour ?></strong> :
                         <?= creneaux_html(CRENEAUX[$k]); ?>
                     </li>
                 <?php endforeach; ?>
--- functions.php
+++ functions.php
@@ -22,8 +22,9 @@ function radio(string $name, string $value, array $data): string
 HTML;
 }
 
-function creneaux_html(array $creneaux) {
-    if (empty($creneaux) === 0){
+function creneaux_html(array $creneaux)
+{
+    if (empty($creneaux)) {
         return 'Fermé';
     }
     $phrases = [];
@@ -31,4 +32,16 @@ function creneaux_html(array $creneaux) {
         $phrases[] = "de <strong>{$creneau[0]}h</strong> à <strong>{$creneau[1]}h</strong>";
     }
     return 'Ouvert ' . implode(' et ', $phrases);
+}
+
+function in_creneaux(int $heure, array $creneaux): bool
+{
+    foreach ($creneaux as $creneau) {
+        $debut = $creneau[0];
+        $fin = $creneau[1];
+        if ($heure >= $debut && $heure < $fin) {
+            return true;
+        }
+    }
+    return false;
 }
