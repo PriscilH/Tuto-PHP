4faa4e4b1a6cd7897aa9aea981e5f98dbe0f6f46
Priscilla <comptepro@mbpdepriscilla.home>
2024-03-15 18:33:52 +0100

    fonction pour lire un fichier

--- cli/test.php
+++ cli/test.php
@@ -1,5 +1,9 @@
 <?php
 // Chemin absolu du fichier
 $fichier = __DIR__ . DIRECTORY_SEPARATOR . 'demo.txt';
+
 // On créer le fichier demo avec les données Salut les gens
-file_put_contents($fichier, 'Salut les gens');
+// file_put_contents($fichier, ' comment allez-vous ?', FILE_APPEND);
+
+// Fonction pour lire le fichier
+echo file_get_contents($fichier);
