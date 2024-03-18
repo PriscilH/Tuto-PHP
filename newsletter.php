5577b882bcacfbdcd050e1604932514244db3762
Priscilla <comptepro@mbpdepriscilla.home>
2024-03-18 20:10:05 +0100

    add form in newsletter page

--- newsletter.php
+++ newsletter.php
@@ -1,3 +1,14 @@
 <?php 
 require 'elements/header.php';
 ?>
+
+<h1>S'inscrire à la Newsletter !</h1>
+
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam illum rerum, inventore sunt tempore soluta magni possimus assumenda molestiae dolor voluptatum provident perspiciatis optio ipsum perferendis! Enim consequatur modi necessitatibus?</p>
+
+<form action="/newsletter.php" method="post" class="form-inline">
+    <div class="form-group">
+        <input type="email" placeholder="Entrer votre email" required>
+    </div>
+    <button type="submit" class="btn btn-primary">S'inscrire</button>
+</form>
