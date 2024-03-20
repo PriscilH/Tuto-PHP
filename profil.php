358942e7e4081ed2bccc06a1cf6a6bae2d666a69
Priscilla <comptepro@mbpdepriscilla.home>
2024-03-20 19:05:10 +0100

    add new section with birthday select

--- profil.php
+++ profil.php
@@ -1,17 +1,19 @@
 <?php
 $title = "Page profil";
-if (!empty($_GET['action']) && $_GET['action'] === 'deconnecter') {
-    unset($_COOKIE['user']);
-    setcookie('user', '', time() - 10);
-}
+// Exemple 1
+// if (!empty($_GET['action']) && $_GET['action'] === 'deconnecter') {
+//     unset($_COOKIE['user']);
+//     setcookie('user', '', time() - 10);
+// }
 $nom = null;
-if (!empty($_COOKIE['user'])) {
-    $nom = $_COOKIE['user'];
-}
-if (!empty($_POST['nom'])) {
-    setcookie('user', $_POST['nom']);
-    $nom = $_POST['nom'];
-}
+// if (!empty($_COOKIE['user'])) {
+//     $nom = $_COOKIE['user'];
+// }
+// if (!empty($_POST['nom'])) {
+//     setcookie('user', $_POST['nom']);
+//     $nom = $_POST['nom'];
+// }
+
 require 'elements/header.php';
 ?>
 
@@ -21,13 +23,18 @@ require 'elements/header.php';
         <h1>Bonjour <?= isset($nom) ? htmlentities($nom) : "" ?></h1>
         <a href="profil.php?action=deconnecter">Se déconnecter</a>
     <?php else : ?>
-        
+
         <form action="" method="post">
             <div class="form-group">
-                <input class="form-control" name="nom" placeholder="Entrer votre nom">
+                <label for="birthday">Section adultes, entrer votre année de naissance :</label>
+                <select name="birthday" class="form-control">
+                    <?php for ($i = 2012; $i > 1990; $i--) : ?>
+                        <option value="<?= $i ?>"><?= $i ?></option>
+                    <?php endfor ?>
+                </select>
             </div>
-            <button class="btn btn-primary mt-3">
-                Se connecter
+            <button type="submit" class="btn btn-primary mt-3">
+                Envoyer
             </button>
         </form>
 </div>
