4148e2068b8defb842b5a46ee950d3fe24a113de
Priscilla <comptepro@mbpdepriscilla.home>
2024-03-21 18:35:25 +0100

    add compteur journalier

--- functions/compteur.php
+++ functions/compteur.php
@@ -2,6 +2,13 @@
 function ajouter_vue()
 {
     $fichier = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'compteur';
+    $fichier_journalier = $fichier . '-' . date('Y-m-d');
+    increment_compteur($fichier);
+    increment_compteur($fichier_journalier);
+}
+
+function increment_compteur(string $fichier): void
+{
     $compteur = 1;
     if (file_exists($fichier)) {
         $compteur = (int)file_get_contents($fichier);
