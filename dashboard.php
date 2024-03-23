c4346afb1a65660ccffa1f29cc86c90b4365b9bc
Priscilla <comptepro@mbpdepriscilla.home>
2024-03-23 11:31:37 +0100

    add array month in dashboard

--- dashboard.php
+++ dashboard.php
@@ -2,18 +2,43 @@
 require 'functions/compteur.php';
 $total = nombre_vues();
 $annee = (int)date('Y');
-$annee_select = empty($_GET['annee']) ? $annee : (int)$_GET['annee'];
+$annee_select = empty($_GET['annee']) ? null : (int)$_GET['annee'];
+$mois_select = empty($_GET['mois']) ? null : (int)$_GET['mois'];
+$mois = [
+    '01' => 'Janvier',
+    '02' => 'Février',
+    '03' => 'Mars',
+    '04' => 'Avril',
+    '05' => 'Mai',
+    '06' => 'Juin',
+    '07' => 'Juillet',
+    '08' => 'Août',
+    '09' => 'Septembre',
+    '10' => 'Octobre',
+    '11' => 'Novembre',
+    '12' => 'Décembre'
+];
+
 require 'elements/header.php';
 ?>
 
 <div class="container mt-3">
     <div class="row">
         <div class="col-md-4">
-<div class="list-group">
-    <?php for ($i = 0; $i < 5; $i++): ?>
-    <a class="list-group-item <?= $annee - $i === $annee_select ? 'active' : ''; ?>"  href="dashboard.php?annee=<?= $annee - $i ?>"><?= $annee - $i ?></a>
-    <?php endfor ?>
-</div>
+            <div class="list-group">
+                <?php for ($i = 0; $i < 5; $i++) : ?>
+                    <a class="list-group-item <?= $annee - $i === $annee_select ? 'active' : ''; ?>" href="dashboard.php?annee=<?= $annee - $i ?>"><?= $annee - $i ?></a>
+                    <?php if ($annee - $i === $annee_select) : ?>
+                        <div class="list-group">
+                            <?php foreach ($mois as $numero => $nom) : ?>
+                                <a class="list-group-item" <?= $numero === $mois_select ? 'active' : '' ?> href="dashboard.php?annee<?= $annee_select ?>&mois=<?= $numero ?>">
+                                    <?= $nom ?>
+                                </a>
+                            <?php endforeach ?>
+                        </div>
+                    <?php endif ?>
+                <?php endfor ?>
+            </div>
         </div>
         <div class="col-md-6">
             <div class="card">
