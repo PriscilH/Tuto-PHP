3ea3741a0f5ba0ccee46515f7df93c1d06541421
Priscilla <comptepro@mbpdepriscilla.home>
2024-03-23 19:08:10 +0100

    add form connect in login page

--- login.php
+++ login.php
@@ -1,12 +1,14 @@
 <?php
 require 'elements/header.php';
 ?>
-<form action="" method="post">
-    <div class="form-group">
-        <input class="form-control" type="text" name="username" placeholder="Nom d'utilisateur">
-    </div>
-    <div class="form-group">
-        <input class="form-control" type="password" name="password" placeholder="Votre mot de passe">
-    </div>
-    <button type="submit" class="btn btn-primary mt-3">Se connecter</button>
-</form>
+<div class="container">
+    <form class="mx-auto p-3" action="" method="post">
+        <div class="form-group mt-3 ">
+            <input class="form-control" type="text" name="username" placeholder="Nom d'utilisateur">
+        </div>
+        <div class="form-group">
+            <input class="form-control mt-3" type="password" name="password" placeholder="Votre mot de passe">
+        </div>
+        <button type="submit" class="btn btn-primary mt-3">Se connecter</button>
+    </form>
+</div>
