e63458b0209f9071baa48d26c35bbea99f766b60
Priscilla <comptepro@mbpdepriscilla.home>
2024-03-24 18:39:25 +0100

    add condition for connectuser

--- login.php
+++ login.php
@@ -1,8 +1,30 @@
 <?php
+$error = null;
+if (!empty($_POST['username']) && !empty($_POST['password'])) {
+    if ($_POST['username'] === 'John' && $_POST['password'] === 'Doe') {
+        session_start();
+        $_SESSION['connecte'] = 1;
+        header('Location: /dashboard.php');
+        exit();
+    } else {
+        $error = "Identifiants incorrects";
+    }
+}
+
 require 'elements/header.php';
 ?>
+
+<div class="container mt-3">
+    <?php if ($error) : ?>
+        <div class="alert alert-danger">
+            <?= $error ?>
+        </div>
+    <?php endif; ?>
+</div>
+
+
 <div class="container">
-    <form class="mx-auto p-3" action="" method="post">
+    <form class="mx-auto " action="" method="post">
         <div class="form-group mt-3 ">
             <input class="form-control" type="text" name="username" placeholder="Nom d'utilisateur">
         </div>
