aed73d5c35184c1a21d9d3abdb36f54e0fe2efef
Priscilla <comptepro@mbpdepriscilla.home>
2024-06-10 19:05:06 +0200

    la méthode statique

--- cli/test.php
+++ cli/test.php
@@ -1,9 +1,7 @@
 <?php
-require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'class' . DIRECTORY_SEPARATOR . 'Creneau.php';
-$creneau = new Creneau(9, 12);
-$creneau2 = new Creneau(14, 16);
-$creneau->intersect($creneau2);
-echo $creneau->toHTML();
+require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'class' . DIRECTORY_SEPARATOR . 'Form.php';
+echo Form::checkbox('demo', 'Demo', []);
+
 
 
 // php cli/test.php
